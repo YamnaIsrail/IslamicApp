2de2e5ad97ad08b0202b85f282ed8847c27bfb84
yum <yamnaisrailkhan@gmail.com>
2024-03-01 23:11:14 +0500

    Changed the color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:islam_app/screens/onboarding_screens/onboarding_1.dart';
+import 'package:islam_app/widgets/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Ramadan App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xffd6a75f)),
         useMaterial3: true,
       ),
       home: OnBoarding(),
